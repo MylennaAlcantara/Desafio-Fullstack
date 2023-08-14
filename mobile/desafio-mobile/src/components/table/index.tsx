79809481b5cc4c0eafd11a94f596304044d6c93b
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2023-08-14 18:00:50 -0300

    commit

--- mobile/desafio-mobile/src/components/table/index.tsx
+++ mobile/desafio-mobile/src/components/table/index.tsx
@@ -1,15 +1,72 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet,Text } from "react-native"
+import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
+import { dadoTipo } from "../../screens/listaProfissional";
 
-export const Table = () => {
+const CONTENT = {
+    tableHead: ['Column 1', 'Column 2', 'Column 3'],
+    tableData: [
+      ['1', '2', '3'],
+      ['a', 'b', 'c'],
+      ['1', '2', '3'],
+      ['a', 'b', 'c'],
+    ],
+};
+
+export const Tabela = ({ dados }: { dados: dadoTipo }) => {
     return(
-        <View style={Styles.container}>
+        <View style={styles.container}>
+            {dados.map((i)=> <Text>{i.nome}</Text>)}
+            <Table style={{ width: "90%", margin: "auto" }}>
+                <Row
+                data={CONTENT.tableHead}
+                flexArr={[1, 1, 1]}
+                style={styles.head}
+                textStyle={styles.text}
+                />
+                <TableWrapper style={styles.wrapper}>
+                    <Rows
+                        data={CONTENT.tableData}
+                        flexArr={[1, 2, 1]}
+                        style={styles.row}
+                        textStyle={styles.textRow}
+                    />
+                </TableWrapper>
+            </Table>
         </View>
     )
 }
 
-const Styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
+        width: "100%",
         height: 550,
-        backgroundColor: "yellow"
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    head: { 
+        height: 40, 
+        backgroundColor: '#007bff',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    wrapper: { 
+        flexDirection: 'row' 
+    },
+    title: { 
+        flex: 1, 
+    },
+    row: { 
+        height: 28,
+        borderBottomColor: "grey",
+        borderBottomWidth: 1,
+        justifyContent: "center",
+        alignItems: "center", 
+    },
+    text: { 
+        textAlign: 'center',
+        color: "#f9f8fc"
+    },
+    textRow: {
+        textAlign: 'center',
     }
 })
--- mobile/desafio-mobile/src/screens/listaProfissional/index.tsx
+++ mobile/desafio-mobile/src/screens/listaProfissional/index.tsx
@@ -1,13 +1,36 @@
 import { ScrollView, StyleSheet } from "react-native"
 import { Header, Props } from "../../components/header"
 import { Footer } from "../../components/footer"
-import { Table } from "../../components/table"
+import { Tabela } from "../../components/table"
+import { useEffect, useState } from "react"
+
+export type dadoTipo = [{
+    id: Number,                
+    nome: String,
+    telefone: String,  
+    email: String,    
+    tipoDeProfissional: Number,
+    situacao: Boolean,
+    updatedAt: String,
+    createdAt: String, 
+}];
 
 export const Profissional = ({navigation}: Props) =>{
+    const [dados, setDados] = useState<dadoTipo | []>([]);
+
+    useEffect(()=>{
+        async function fetchData() {
+            const response = await fetch("http://localhost:8080/profissional");
+            const data: dadoTipo = await response.json();
+            setDados(data);
+        }    
+        fetchData();
+    },[]);
+
     return(
         <ScrollView style={Style.container}>
             <Header navigation={navigation}/>
-            <Table/>
+            <Tabela dados={dados as dadoTipo}/>
             <Footer/>
         </ScrollView>
     )
@@ -16,5 +39,6 @@ export const Profissional = ({navigation}: Props) =>{
 const Style = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "white"
     }
 })
