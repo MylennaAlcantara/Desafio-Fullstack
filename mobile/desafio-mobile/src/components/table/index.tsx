3777b7ac488711b5f22f91a3ac90da46ba5ce473
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2023-08-15 23:22:00 -0300

    tabela com flatlist

--- mobile/desafio-mobile/src/components/table/index.tsx
+++ mobile/desafio-mobile/src/components/table/index.tsx
@@ -1,38 +1,90 @@
 import { View, StyleSheet,Text, ScrollView } from "react-native"
 import { Table, TableWrapper, Row, Rows, Col } from 'react-native-table-component';
 import { dadoTipo } from "../../screens/listaProfissional";
+import {FlatList} from "react-native";
 
+interface Props {
+    dados: dadoTipo[];
+}
 
-export const Tabela = ({ dados }: { dados: dadoTipo }) => {
+export const Tabela = ({ dados }: {dados: dadoTipo}) => {
+    return (
+        <View style={styles.container}>
+            <ScrollView horizontal={true}>
+                <View style={styles.row}>
+                    <Text style={styles.textRowHeader}>ID</Text>
+                    <Text style={styles.textRowHeader}>Nome</Text>
+                    <Text style={styles.textRowHeader}>Profissão</Text>
+                    <Text style={styles.textRowHeader}>Telefone</Text>
+                    <Text style={styles.textRowHeader}>Email</Text>
+                </View>
+                {dados.map((item, index) => (
+                    <View key={index} style={styles.row}>
+                        <Text style={styles.textRow}>{String(item.id)}</Text>
+                        <Text style={styles.textRow}>{item.nome}</Text>
+                        <Text style={styles.textRow}>{String(item.tipoDeProfissional)}</Text>
+                        <Text style={styles.textRow}>{item.telefone}</Text>
+                        <Text style={styles.textRow}>{item.email}</Text>
+                    </View>
+                ))}
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 550,
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+    },
+    textRowHeader: {
+        flex: 1,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    textRow: {
+        flex: 1,
+        textAlign: 'center',
+    },
+});
+
+export default Tabela;
+
+/*export const Tabela = ({ dados }: { dados: dadoTipo }) => {
     const CONTENT = {
         tableHead: ['ID', 'Nome', 'Profissão', 'Telefone', 'Email'],
-        /*tableData: [
-          ['1', '2', '3'],
-          ['a', 'b', 'c'],
-          ['1', '2', '3'],
-          ['a', 'b', 'c'],
-        ],*/
-        tableData: [dados.map((cell)=> [cell.id, cell.nome, cell.tipoDeProfissional, cell.telefone, cell.email])]
+        tableData: [dados.map((cell)=> [cell.id, cell.nome, cell.tipoDeProfissional, cell.telefone, cell.email],)]
     };
     return(
         <View style={styles.container}>
-            {dados.map((i)=> <Text>{i.nome}</Text>)}
-            <Table style={{ width: "100%", margin: "auto" }}>
+            <ScrollView horizontal={true} style={{ flexDirection: "row", maxWidth: "100%", maxHeight: "90%" }}>
+            <Table style={{ width: "100%"}}>
                 <Row
                 data={CONTENT.tableHead}
-                flexArr={[1, 3, 1, 2, 2]}
+                flexArr={[1, 1, 1, 1, 1]}
                 style={styles.head}
                 textStyle={styles.text}
                 />
-                <TableWrapper style={styles.wrapper}>
-                    <Rows
-                        data={CONTENT.tableData}
-                        flexArr={[1, 3, 1, 2, 2]}
-                        style={styles.row}
-                        textStyle={styles.textRow}
-                    />
-                </TableWrapper>
+                {CONTENT.tableData.map((rowData, index) => {
+                    return(
+                        <Rows
+                            key={index+1}
+                            data={rowData}
+                            flexArr={[1, 1, 1, 1, 1]}
+                            style={styles.row}
+                            textStyle={styles.textRow}
+                        />
+                    )
+                })}
             </Table>
+            </ScrollView>
         </View>
     )
 }
@@ -49,9 +101,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#007bff',
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
-    },
-    wrapper: { 
-        flexDirection: 'row' 
     },
     title: { 
         flex: 1, 
@@ -70,4 +119,4 @@ const styles = StyleSheet.create({
     textRow: {
         textAlign: 'center',
     }
-})
+})*/
